62e52c8e485821518153fb93d13dd3cdb5e24eec
Pi <piisthebestthingintheuniverse@gmail.com>
2024-07-07 00:15:37 -0700

    error catching

--- index.js
+++ index.js
@@ -14,48 +14,70 @@ function sendWebhook(url, msg) {
 					content: msg,
 				}),
 			}).then(() => resolve());
-		} catch {}
+		} catch {
+			resolve();
+		}
 	});
 }
 
 function getAiResponse(user, msg) {
 	return new Promise((resolve) => {
-		fetch(process.env.API_ENDPOINT, {
-			headers: {
-				accept: "text/plain",
-				"accept-language": "en-US,en;q=0.9",
-				"content-type": "application/json",
-			},
-			body: JSON.stringify({
-				UserId: user,
-				ConversationId: `${user}-${uniqueId}`,
-				UserInput: msg,
-			}),
-			method: "POST",
-		})
-			.then((res) => res.json())
-			.then((json) => resolve(json.response));
+		try {
+			fetch(process.env.API_ENDPOINT, {
+				headers: {
+					accept: "text/plain",
+					"accept-language": "en-US,en;q=0.9",
+					"content-type": "application/json",
+				},
+				body: JSON.stringify({
+					UserId: user,
+					ConversationId: `${user}-${uniqueId}`,
+					UserInput: msg,
+				}),
+				method: "POST",
+			})
+				.then((res) => res.json())
+				.then((json) => resolve(json.response));
+		} catch {
+			resolve("...");
+		}
 	});
 }
 
 function bot1(reply) {
-	getAiResponse(process.env.BOT1_NAME, reply).then((res) => {
-		sendWebhook(process.env.BOT1_WEBHOOK, res).then(() => {
-			setTimeout(() => {
-				bot2(res);
-			}, 2000);
+	getAiResponse(process.env.BOT1_NAME, reply)
+		.then((res) => {
+			sendWebhook(process.env.BOT1_WEBHOOK, res)
+				.then(() => {
+					setTimeout(() => {
+						bot2(res);
+					}, 2000);
+				})
+				.catch(() => {
+					bot2("...");
+				});
+		})
+		.catch(() => {
+			bot2("...");
 		});
-	});
 }
 
 function bot2(reply) {
-	getAiResponse(process.env.BOT2_NAME, reply).then((res) => {
-		sendWebhook(process.env.BOT2_WEBHOOK, res).then(() => {
-			setTimeout(() => {
-				bot1(res);
-			}, 2000);
+	getAiResponse(process.env.BOT2_NAME, reply)
+		.then((res) => {
+			sendWebhook(process.env.BOT2_WEBHOOK, res)
+				.then(() => {
+					setTimeout(() => {
+						bot1(res);
+					}, 2000);
+				})
+				.catch(() => {
+					bot1("...");
+				});
+		})
+		.catch(() => {
+			bot1("...");
 		});
-	});
 }
 
 bot1("hello");
